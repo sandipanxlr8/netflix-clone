511600141a22c34aeed51ba668dbd35edfb12d27
sandipan <kalita519@gmail.com>
2021-02-24 02:41:12 +0530

    fix bugs

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,16 +1,17 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 //import { seedDatabase } from '../seed';
 
+//copy the below values from your firebase console
 const config = {
-  apiKey: "AIzaSyDYYILmNGgv5GRX2FyQMfUTBB-D4Jh6uW8",
-  authDomain: "netflix-clone-d32e6.firebaseapp.com",
-  databaseURL: "https://netflix-clone-d32e6.firebaseio.com",
-  projectId: "netflix-clone-d32e6",
-  storageBucket: "netflix-clone-d32e6.appspot.com",
-  messagingSenderId: "461889082650",
-  appId: "1:461889082650:web:7043ca8bb56639111588a3"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebase = Firebase.initializeApp(config);
